c9570b06325497ee4d81f2a45fb7d89e8e42a9df
talhahzahid <talhazahid415@gmail.com>
2024-10-02 15:42:07 +0500

    Hello World

---
+++ helloworld/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <h1 className='flex justify-center h-[100vh] items-center text-4xl font-semibold'>Hello World</h1>
+    </>
+  )
+}
+
+export default App
